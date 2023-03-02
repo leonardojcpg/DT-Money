5f9f029bca97c7d6095b8696b3a6646ba13b62fd
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-03-01 21:05:26 -0300

    adding memo component

--- src/Components/SearchForm/index.tsx
+++ src/Components/SearchForm/index.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../Contexts/TransactionsContext'
+import { memo } from 'react'
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,7 +13,7 @@ const searchFormSchema = z.object({
 // me retorna qual a tipagem dos campos do formulario
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export const SearchForm = () => {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -45,3 +46,5 @@ export const SearchForm = () => {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
